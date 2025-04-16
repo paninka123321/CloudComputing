1247cb27b208f5e4332232e7cef2520003acf684
paninka123321 <paulinka_k2000@wp.pl>
2025-04-16 13:16:18 +0200

    backend connected to postgresql

--- backend/core/management/commands/import_nauczyciele.py
+++ backend/core/management/commands/import_nauczyciele.py
@@ -1,23 +1,26 @@
 # core/management/commands/import_nauczyciele.py
 import csv
 from django.core.management.base import BaseCommand
-from core.models import Nauczyciel 
+from core.models import Nauczyciel, Klasa
 
 class Command(BaseCommand):
     help = 'Import nauczyciele from CSV'
 
     def handle(self, *args, **kwargs):
         with open('/Users/paulinakulczyk/Desktop/Magisterka/Cloud Computing/psychology-app/database/csvki/nauczyciele.csv', newline='', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile)
+            reader = csv.DictReader(csvfile, delimiter=';')
             for row in reader:
+                print(row) # for debugging
+                # Get or create the Klasa instance
+                klasa_obj, created = Klasa.objects.get_or_create(nazwa=row['klasa'])
                 Nauczyciel.objects.create(
                     id_nauczyciela=row['id_nauczyciela'],
-                    imie=row['imie'],
+                    imie=row['imię'],
                     nazwisko=row['nazwisko'],
-                    wyksztalcenie=row['wyksztalcenie'],
+                    wyksztalcenie=row['wykształcenie'],
                     nauczany_przedmiot=row['nauczany_przedmiot'],
                     wiek=row['wiek'],
                     plec=row['plec'],
-                    klasa=row['klasa'],
-                    nazwa_szkoly=row['nazwa_szkoly']
+                    klasa=klasa_obj,
+                    nazwa_szkoly=row['nazwa_szkoły']
                 )
