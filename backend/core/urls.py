7d72b6fcccd73221b9345ab4f1f80549b122be80
paninka123321 <paulinka_k2000@wp.pl>
2025-04-24 19:40:42 +0200

    connecting games with backend and postgressql
    Former-commit-id: 07aa305eb25bb7ffd487fac61f8b0388edcfb76c

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("hello/", hello_api),
-        path('api/shapes/', ShapesDatasetView.as_view(), name='shapes'),
+    path('api/shapes/', ShapesDatasetView.as_view(), name='shapes'),
     path('api/emotions/', EmotionsDatasetView.as_view(), name='emotions'),
     path('api/writings/', WritingsDatasetView.as_view(), name='writings'),
 ]
