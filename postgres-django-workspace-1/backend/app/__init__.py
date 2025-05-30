d5529c47d97f8a6547f57c3dea918108f3f24471
poglodjan <100129357+poglodjan@users.noreply.github.com>
2025-05-30 17:33:15 +0200

    Delete postgres-django-workspace-1 directory

--- postgres-django-workspace-1/backend/app/__init__.py
+++
@@ -1,2 +0,0 @@
-# filepath: /Users/janpoglod/Desktop/studies/cloud/postgres-django-workspace/backend/app/__init__.py
-# This file is intentionally left blank.
--- postgres-django-workspace-1/backend/backend/__init__.py
+++
@@ -1,2 +0,0 @@
-# filepath: /Users/janpoglod/Desktop/studies/cloud/postgres-django-workspace/backend/backend/__init__.py
-# This file is intentionally left blank.
